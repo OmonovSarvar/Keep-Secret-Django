1c0002ff095faf6fefe652c6f9b061a3eb33ccd4
OmonovSarvar <sarvaromon33@gmail.com>
2023-12-19 20:20:43 +0500

    Model created and added to models.py

--- secret_app/models.py
+++ secret_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionModel(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.TextField()
+    hashtag = models.CharField(max_length=50)
+    likes = models.ManyToManyField(User, related_name='tweet_like')
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-datetime"]
+
+    def __str__(self):
+        return self.question
+
+
+class CommentModel(models.Model):
+    question = models.ForeignKey(QuestionModel, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-datetime"]
+
+    def __str__(self):
+        return self.comment
