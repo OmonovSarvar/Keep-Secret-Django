9975c8f762f7770ec1559f08e58b2e8fd45abfe8
OmonovSarvar <sarvaromon33@gmail.com>
2023-12-19 22:48:00 +0500

    Models.py updated Model created for Users

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class UserModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='profile_images', default='profile.png')
+
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return f"{self.user.username}"
