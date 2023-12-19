8e02d065546ce1e0f8bced50856af46650157b1c
OmonovSarvar <sarvaromon33@gmail.com>
2023-12-19 22:57:55 +0500

    Views Updated

--- users_app/forms.py
+++ users_app/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class SignUp(UserCreationForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['last_name', 'first_name', 'email', 'username', 'password1', 'password2']
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import RegisterForm
+
 
 # Create your views here.
+class Login(LoginView):
+    template_name = 'users_app/login.html'
+    next_page = 'question'
+
+
+class Logout(LogoutView):
+    next_page = 'login'
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    login_url = 'login'
+    template_name = 'users_app/profile.html'
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegisterForm()
+    return render(request, 'users_app/signup.html', context={'form': form})
